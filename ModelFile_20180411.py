bb90f4f9bba9d903559829fe3bec15018fc5488c
Lelsler <laura.elsler@su.se>
2018-07-13 17:51:04 +0200

    timeseries C/pf model and runmodel

--- ModelFile_20180411.py
+++ ModelFile_20180411.py
@@ -1,14 +1,48 @@
+#### Import packages ###########################################################
+import scipy.stats as st
+from pandas import *
+
+#### Model w/o relationships ###################################################
+flag = 1 # 0 = NoR model; 1 = Rmodel
+
+#### Load dataset  #############################################################
+df1 = pd.read_excel('./Dropbox/PhD/Resources/P2/Squid/Laura/R3_data.xlsx', sheetname='Sheet1')
+#! load columns
+y = df1['year'] #
+pe = df1['pe_MXNiat'] #
+pf = df1['pf_MXNiat'] #
+ct = df1['C_t'] #
+ssh = df1['essh_avg'] #
+ml = df1['ML'] #
+ys = df1['y_S'] #
+
+### New max time
+tmax = len(y)
+
 ### Define Model ###############################################################
 def model(b1, b2, b3, n1, n2, l1, l2, a1, g, K, m, f, B_h, B_f, h1, h2, gamma, beta, c_p, w_m, flag):
     for t in np.arange(1,tmax):
         # isotherm depth
         tau[t]= b1 +b2 *np.cos(t) + b3 *np.sin(t)
-        # mantle length
-        ML[t]= n1 *tau[t] + n2
-        # catchability
-        q[t]= l1 *tau[t] +l2
+        # mantle length and catchability
+        if ml[t] == 1:
+            q[t]= l1 *tau[t] +l2
+        else:
+            ML[t]= ml[t]
+            q[t]= 0.0018 *ML[t] - 0.0318
+
         # migration of squid
-        y_S[t] = a1 *np.exp(tau[t]-b1)
+        if ys[t] == 1:
+            y_S[t] = a1 *np.exp(tau[t]-b1)
+        else:
+            y_S[t]= ys[t]
+        if y_S[t] > 1:
+            y_S[t] = 1
+            print "yS high"
+        elif y_S[t] < 0:
+            y_S[t] = 0
+            print "yS low"
+
         # trader cooperation
         R_tt[t] = (1-y_S[t])
         # squid population
@@ -19,6 +53,13 @@ def model(b1, b2, b3, n1, n2, l1, l2, a1, g, K, m, f, B_h, B_f, h1, h2, gamma, b
         Escal[t] = E[t-1] + p_f[t-1] *q[t-1] *E[t-1] *S[t-1] -c_t[t-1] *(E[t-1]/(B_h+B_f)) # c_t is per trip so we need to upscale E hr > fisher > trip
         # fishing effort scaled
         E[t] = h1 *Escal[t] + h2 # Escal €[-3,10E+09; 1,60E+09]
+        if E[t] > 1:
+            E[t] = 1
+            print "E high"
+        elif E[t] < 0:
+            E[t] = 0
+            print "E low"
+
         # catch
         C[t] = q[t] *E[t] *S[t]
         # export price
@@ -36,5 +77,6 @@ def model(b1, b2, b3, n1, n2, l1, l2, a1, g, K, m, f, B_h, B_f, h1, h2, gamma, b
 
         # revenue of fishers
         R[t] = C[t] *p_f[t] - c_t[t-1] *(E[t-1]/(B_h+B_f))
+
         print t, tau[t], ML[t], q[t], y_S[t], S[t], c_t[t], E[t], C[t], p_e[t], p_f[t], R[t]
     return tau, ML, q, y_S, R_tt, S, c_t, E, C, p_e, p_f, R
--- RunModel_20180411.py
+++ RunModel_20180411.py
@@ -1,30 +1,69 @@
-#### Import packages ###########################################################
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import seaborn as sns
-import pandas as pd
-
-#### model w/o relationships ###################################################
-flag = 1 # 0 = NoR model; 1 = Rmodel
+##### Initiate arrays ##########################################################
+sim = np.arange(0,100) # number of simulations
+x = np.zeros(12) # set array to save parameters
+par = np.zeros((sim.shape[0],x.shape[0])) # matrix to save parameter values of each simulation
+cat = np.zeros((sim.shape[0],tau.shape[0])) # matrix to save catches in each time period of each simulation
+pri = np.zeros((sim.shape[0],tau.shape[0])) # matrix to save prices in each time period of each simulation
 
 ##### Run the model ############################################################
-OUT = np.zeros(tau.shape[0])
-OUT1 = np.zeros(tau.shape[0])
+for j in range(0,sim.shape[0]): # draw randomly a float in the range of values for each parameter
+    # a1 = np.random.uniform(2E-08, 3E-08) # parameter ranges
+    # a2 = np.random.uniform(0.2, 9E-16)
+    b1 = np.random.uniform(38.750, 42.1)
+    b2 = np.random.uniform(-3.987, -6.9)
+    b3 = np.random.uniform(11.478, 16.4)
+    beta = np.random.uniform(0.01, 0.1)
+    c_p = np.random.uniform(1000, 2148)
+    g = np.random.uniform(0, 2.9)
+    gamma = np.random.uniform(20000, 51000)
+    # l1 = np.random.uniform(-0.0005, -0.0122)
+    # l2 = np.random.uniform(0.0317, 0.7927)
+    m = np.random.uniform(2368793, 8450159)
+    w_m = np.random.uniform(11956952, 28108539)
 
-for i in np.arange(0,tau.shape[0]):
-        tau, ML, q, y_S, R_tt, S, c_t, E, C, p_e, p_f, R = model(b1, b2, b3, n1, n2, l1, l2, a1, g, K, m, f, B_h, B_f, h1, h2, gamma, beta, c_p, w_m, flag)
-        OUT[i]= p_f[i]
-        OUT1[i]= C[i]
+    x = [a1, b1, b2, b3, beta, c_p, g, gamma, l1, l2, m , w_m]
+    par[j] = x
 
-##### Save stuff ###############################################################
-###! AS NPY
+    OUT = np.zeros(tau.shape[0])
+    OUT1 = np.zeros(tau.shape[0])
+
+    for i in np.arange(1,tmax):
+            tau, ML, q, y_S, R_tt, S, c_t, E, C, p_e, p_f, R = model(b1, b2, b3, n1, n2, l1, l2, a1, g, K, m, f, B_h, B_f, h1, h2, gamma, beta, c_p, w_m, flag)
+            OUT[i]= p_f[i]
+            OUT1[i]= C[i]
+            pri[j,i] = OUT[i]
+            cat[j,i] = OUT1[i]
+
+
+lowC = np.zeros(y.shape[0]) # initiate variables for 95% confidence interval
+highC = np.zeros(y.shape[0])
+meanC = np.zeros(y.shape[0])
+lowP = np.zeros(y.shape[0])
+highP = np.zeros(y.shape[0])
+meanP = np.zeros(y.shape[0])
+
+for h in range(0,y.shape[0]): # calculate the 95% confidence interval
+    z = cat[:,h]
+    lowC[h] = np.nanmean(z) - ((1.96 * np.nanstd(z))/np.sqrt(np.count_nonzero(~np.isnan(z))))
+    highC[h] = np.nanmean(z) + ((1.96 * np.nanstd(z))/np.sqrt(np.count_nonzero(~np.isnan(z))))
+    meanC[h] = np.nanmean(z)
+    zeta = pri[:,h]
+    lowP[h] = np.nanmean(zeta) - ((1.96 * np.nanstd(zeta))/np.sqrt(np.count_nonzero(~np.isnan(zeta))))
+    highP[h] = np.nanmean(zeta) + ((1.96 * np.nanstd(zeta))/np.sqrt(np.count_nonzero(~np.isnan(zeta))))
+    meanP[h] = np.nanmean(zeta)
+
+##### Save data  ###############################################################
 if flag == 0:
-    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/ModelNoR_pf_20180411.npy", OUT)
-    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/ModelNoR_C_20180411.npy", OUT1)
-    print "model without relationships"
-
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support2_95_NoR_lowC.npy", lowC)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support2_95_NoR_highC.npy", highC)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_NoR_lowP.npy", lowP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_NoR_highP.npy", highP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_NoR_meanP.npy", meanP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_NoR_meanC.npy", meanC)
 if flag == 1:
-    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/ModelR_pf_20180411.npy", OUT)
-    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/ModelR_C_20180411.npy", OUT1)
-    print "model with relationships"
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support2_95_R_lowC.npy", lowC)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support2_95_R_highC.npy", highC)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_R_lowP.npy", lowP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_R_highP.npy", highP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_R_meanP.npy", meanP)
+    np.save("./Dropbox/PhD/Resources/P2/Squid/CODE/PY/DATA/R1support1_95_R_meanC.npy", meanC)
