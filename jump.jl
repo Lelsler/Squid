5504bbebbdefd34615c6682e59dcc14047389aa4
Tim DuBois <tim@neophilus.net>
2018-08-21 15:36:03 +0200

    More additions. Only excluding E and ICs.

--- jump.jl
+++ jump.jl
@@ -47,52 +47,50 @@ tmax = length(df2[:Year]);
 model = JuMP.Model(solver = IpoptSolver(max_iter=10000,print_frequency_iter=500,sb="yes"));
 #model = JuMP.Model(solver = IpoptSolver(sb="yes"));
 
-#@variable(model, b1) # isotherm depth (est)
-#@variable(model, b2) # isotherm depth (est)
-#@variable(model, b3) # isotherm depth (est)
+@variable(model, b1, start=41.750) # isotherm depth (est)
+@variable(model, b2, start=-5.696) # isotherm depth (est)
+@variable(model, b3, start=16.397) # isotherm depth (est)
 @variable(model, 0.0 <= beta <= 1.0) # slope of demand-price function
 @variable(model, 1000.0 <= c_p <= 2148.0) # cost of processing, MXNia/t
-#@variable(model, 0.0 <= g <= 3.2) # population growth rate
+@variable(model, 0.0 <= g <= 3.2) # population growth rate
 @variable(model, 20000.0 <= gamma <= 51000.0) # maximum demand, t
 #@variable(model, 11956952.0 <= w_m <= 28108539.0) # min wage per hour all fleet
 #@variable(model, h1) # E scale
 #@variable(model, h2) # E scale
-#@NLparameter(model, l1 == -0.0028) # q scale
-#@NLparameter(model, l2 == 0.1667) # q scale
+@NLparameter(model, l1 == -0.0028) # q scale
+@NLparameter(model, l2 == 0.1667) # q scale
 #testing
-#@variable(model, a1, start=1/3.4e7)
-#@variable(model, K, start=1208770.0)
+@variable(model, a1, start=1/3.4e7)
+@variable(model, K, start=1208770.0)
 
-#@variable(model, 20.0 <= tau[t=1:tmax] <= 80.0) # temperature
+@variable(model, 20.0 <= tau[t=1:tmax] <= 80.0) # temperature
 @variable(model, 4000.0 <= p_e[t=1:tmax] <= 100_000.0) # export price
 @variable(model, q[t=1:tmax]) # catchability squid population
-#@variable(model, 0.0 <= y_S[t=1:tmax] <= 1.0) # Proportion of squid migration from initial fishing grounds
-#@variable(model, R_tt[t=1:tmax]) # trader cooperation
+@variable(model, 0.0 <= y_S[t=1:tmax] <= 1.0) # Proportion of squid migration from initial fishing grounds
 # c_t is per trip so we need to upscale E hr > fisher > trip
 #@variable(model, Escal[t=1:tmax]) # fishing effort
 @variable(model, 0.0 <= E[t=1:tmax] <= 1.0) # fishing effort
 @variable(model, S[t=1:tmax] >= 0.0) # size of the squid population 18000 , 1200000
 @variable(model, C[t=1:tmax]  >= 0.0) # squid catch 10% of S
+#@variable(model, R_tt[t=1:tmax]) # trader cooperation
 #@variable(model, p_min[t=1:tmax] >= 0.0) # minimum wage 28_000_000
 @variable(model, p_f[t=1:tmax]) # price for fishers
-#@variable(model, dataP[t=1:tmax])
-#@variable(model, dataC[t=1:tmax])
 @variable(model, match)
 
-#@constraint(model, [t=1:tmax], tau[t] == b1+b2*cos.(t)+b3*sin.(t));
+@constraint(model, [t=1:tmax], tau[t] == b1+b2*cos.(t)+b3*sin.(t));
 @NLconstraint(model, [t=1:tmax], p_e[t] == gamma*(C[t])^(-beta));
 #@NLconstraint(model, [t=1:tmax-1], Escal[t+1] == E[t]+p_f[t]*C[t]-c_t*(E[t]/(B_h+B_f)));
 #@constraint(model, [t=1:tmax], E[t] == h1*Escal[t]+h2);
-#@constraint(model, [t=1:tmax], R_tt[t] == 1-y_S[t]);
-#@NLconstraint(model, [t=1:tmax-1], S[t+1] == S[t]+g*S[t]*(1-(S[t]/K))-C[t]);
+@NLconstraint(model, [t=1:tmax-1], S[t+1] == S[t]+g*S[t]*(1-(S[t]/K))-C[t]);
 @NLconstraint(model, [t=1:tmax], C[t] == q[t]*E[t]*S[t]);
-#@NLconstraint(model, [t=1:tmax], y_S[t] == a1*exp(tau[t]-b1));
-#@NLconstraint(model, [t=1:tmax], q[t] == l1*tau[t]+l2);
+@NLconstraint(model, [t=1:tmax], y_S[t] == a1*exp(tau[t]-b1));
+@NLconstraint(model, [t=1:tmax], q[t] == l1*tau[t]+l2);
 #First Model
 @constraint(model, [t=1:tmax], p_f[t] == p_e[t]-c_p);
 #Second model
 #@NLconstraint(model, [t=1:tmax], p_min[t] == (E[t]*w_m)/C[t]);
 #@constraint(model, [t=1:tmax], p_f[t] == (p_e[t]-c_p)*(1-R_tt[t])+R_tt[t]*p_min[t]);
+#@constraint(model, [t=1:tmax], R_tt[t] == 1-y_S[t]);
 
 
 #@constraint(model, C[1] == VolAll[1]); # squid catch
