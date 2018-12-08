1ccd059bb2a2918ddb39d2f62a5c0775610beb6d
Lelsler <laura.elsler@su.se>
2018-12-08 16:36:28 +0100

    changed R3 figure and code to show SST anomaly rather than SSH

--- CODE/R3.py
+++ CODE/R3.py
@@ -23,20 +23,23 @@ df2 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/composi
 yr2 = df2['year']
 C_out = df2['outside_catch']
 
-###### PLOTS ###################################################################
-###! Plot 3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-colmap = cm.ScalarMappable(cmap=cm.hsv)
-colmap.set_array(yr)
-p = ax.scatter(p,C,T,c=cm.hsv(yr), marker='o')
-cb = fig.colorbar(colmap)
-ax.set_xlabel('relative price pf/pe$')
-ax.set_ylabel('catch')
-ax.set_zlabel('sea surface height')
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R3_20180411.png',dpi=500)
-plt.show()
+###### LOAD DATA ###############################################################
+df3 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/Laura/SST_anomaly.xlsx', sheetname='Sheet1')
 
+# load columns
+yr3 = df3['year']
+sst = df3['esst_avg']
+
+# sts = np.array(sst, dtype=pd.Series)
+# tss = np.zeros(25)
+# sts[9:]= tss
+#
+# copyto(sts[9:],tss)
+# sst[9:] = np.empty_like (b)
+# a[:] = b
+
+
+###### PLOTS ###################################################################
 ###! Time series plot
 hfont = {'fontname':'Helvetica'}
 
@@ -44,8 +47,8 @@ hfont = {'fontname':'Helvetica'}
 fig1 = figure()
 # and the first axes using subplot populated with data
 ax1 = fig1.add_subplot(111)
-line1, = ax1.plot(T, color="orange")
-ylabel("SSH anomaly $m$", fontsize=20, **hfont)
+line1, = ax1.plot(sst, color="orange")
+ylabel("SST anomaly $m$", fontsize=20, **hfont)
 xlabel("year", fontsize=20, **hfont)
 ax1.set_xticklabels(np.arange(1991,2017,5))
 ax1.set_xlim(right=2016)
@@ -58,18 +61,34 @@ ax2.yaxis.set_label_position("right")
 # y-axis 2
 ax2.set_ylabel("Catches $tons$", rotation=270, color='k', labelpad=15, fontsize=20, **hfont)
 # for the legend, remember that we used two different axes so, we need to build the legend manually
-plt.legend([line1, line2, line3], ["SSH", "Total catches", "Catches outside Gulf"], fontsize= 11)
-# savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R3_support2.png',dpi=500)
+plt.legend([line1, line2, line3], ["SST anomaly", "Total catches", "Catches outside Gulf"], fontsize= 11)
+savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R3_support2.png',dpi=500)
 show()
 
-###### STATISTICS #################################################################
+###### STATISTICS ##############################################################
 # T = T[2:26]
 # C = C[2:26]
 # C_out = C_out[2:]
+#
+# from scipy.stats import linregress
+# linregress(T, C)
+# linregress(T, C_out)
 
-from scipy.stats import linregress
-linregress(T, C)
-linregress(T, C_out)
+# # to get the coefficient of determination "r-squared:" r_value**2
+# # r-squared (T,C): -0.18584996957060027
+#
+# rvalue=0.028448939264492042**2
 
-# to get the coefficient of determination "r-squared:" r_value**2
-# r-squared (T,C): -0.18584996957060027
+###### 3D PLOT #################################################################
+# ###! Plot 3D
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# colmap = cm.ScalarMappable(cmap=cm.hsv)
+# colmap.set_array(yr)
+# p = ax.scatter(p,C,T,c=cm.hsv(yr), marker='o')
+# cb = fig.colorbar(colmap)
+# ax.set_xlabel('relative price pf/pe$')
+# ax.set_ylabel('catch')
+# ax.set_zlabel('sea surface height')
+# # fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R3_20180411.png',dpi=500)
+# plt.show()
