e132722ed12038baf5ace839a1fdc461c3144800
Lelsler <laura.elsler@su.se>
2019-01-27 10:17:20 +0100

    correct legend names and use new sst for b1b2 scripts

---
+++ CODE/parameter_sweep.py
@@ -0,0 +1,413 @@
+#### Import packages ###########################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+from pandas import *
+
+#### Model w/o relationships ###################################################
+flag = 1 # 0 = BEM; 1 = MLM, # 2 = BLM
+
+### Parameters #################################################################
+tmax = 35 # model run, years
+b0 = -40.901 #
+b1 = 0.020 #
+b2 = 0.165 #
+b3 = -0.287 #
+l1 = -0.0912  #
+l2 = 0.0231 #
+c_t = 107291548 # cost of fishing
+qc = 0.1 # catchability constant
+f = 0 # intercept of trader cooperation
+d = 1 # slope of trader cooperation
+K = 1208770 # carrying capacity
+g = 1.4 # population growth rate
+gamma = 49200 # maximum demand
+beta = 0.0736 # slope of demand-price function
+c_p = 1776.25 # cost of processing
+
+h1 = 2E-10 # scale E
+h2 = 0.6596 # scale E
+
+### Variables ##################################################################
+tau = np.zeros(tmax) # temperature
+q = np.zeros(tmax) # catchability squid population
+ML = np.zeros(tmax) # mantle length
+y_S = np.zeros(tmax) # distance of squid migration from initial fishing grounds
+R_tt = np.zeros(tmax) # trader cooperation
+S = np.zeros(tmax) # size of the squid population
+Escal = np.zeros(tmax) # scale effort
+E = np.zeros(tmax) # fishing effort
+C = np.zeros(tmax) # squid catch
+p_e = np.zeros(tmax) # export price
+p_min = np.zeros(tmax) # minimum wage
+p_f = np.zeros(tmax) # price for fishers
+RFn = np.zeros(tmax) # revenue of fishers normalized
+RF = np.zeros(tmax) # revenue of fishers
+RT = np.zeros(tmax) # revenue of traders
+RA = np.zeros(tmax) # revenue all fishery
+G = np.zeros(tmax) # pay gap between fishers and traders
+
+
+### Initial values #############################################################
+tau[0] = -0.80 # for SSTres
+q[0] = 0.05 # squid catchability
+y_S[0] = 0.5 # proportion of migrated squid
+R_tt[0] = 0.5 # trader cooperation
+S[0] = 610075 # size of the squid population, mean
+E[0] = 0.5 # fishing effort
+C[0] = 60438 # squid catch mean
+p_e[0] = 52035 # mean p_e comtrade, rounded
+p_f[0] = 8997 # mean p_f datamares, rounded
+
+
+################################################################################
+###########################  MODEL FILE  #######################################
+################################################################################
+
+#### Load dataset  #############################################################
+df1 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/R3_data.xlsx', sheetname='Sheet1')
+#! load columns
+y = df1['year'] #
+pe = df1['pe_MXNiat'] #
+pf = df1['pf_MXNiat'] #
+ct = df1['C_t'] #
+ssh = df1['essh_avg'] #
+ml = df1['ML'] #
+ys = df1['y_S'] #
+
+### continuous migration trigger ###############################################
+## calculates migration from a continuous sin/cos function
+xo = np.linspace(1991,2025,1000) # 100 linearly spaced numbers
+#y = np.sin(x)/x # computing the values of sin(x)/x
+yo = 4.1E-15 *np.exp(100*(b2*np.cos(xo)-b3*np.sin(xo)))
+plt.plot(xo,yo) # sin(x)/x
+plt.show()
+
+yo = np.array([0,0,0.9773642085169757,0,0,0,0,0,0.9773642085169757,0,0,0,0,0,0.9773642085169757,0,0,0,0,0,0.9773642085169757,0,0,0,0,0,0, 0.9773642085169757,0,0,0,0,0,0.9773642085169757,0])
+
+### Normalizes q values ########################################################
+## calculates q as normalized value of tau over the time span provided
+qMax = 0 # is reverse bc is reverse to tau values
+qMin = 0.2
+valueScaled = np.zeros(tmax)
+
+def translate(b0, b1, b2, b3, tau, qMin, qMax):
+    tau[t]= b0 +b1 *(t+1990) +b2 *np.cos(t+1990) + b3 *np.sin(t+1990)
+
+    # Figure out how 'wide' each range is
+    tauSpan = max(tau) - min(tau)
+    qSpan = qMax - qMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled[t] = float(tau[t] - min(tau)) / float(tauSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    q = qMin + (valueScaled * qSpan)
+
+    return q, tauSpan, qSpan
+
+for t in np.arange(0,tmax):
+     q, tauSpan, qSpan = translate(b0, b1, b2, b3, tau, qMin, qMax)
+
+tauMin = min(tau)
+
+
+################################################################################
+###########################  MODEL FILE  #######################################
+################################################################################
+
+### Define Model ###############################################################
+def model(b0, b1, b2, b3, l1, l2, qc, qMin, qSpan, tauSpan, tauMin, d, f, g, K, h1, h2, gamma, beta, c_p, c_t, flag):
+    for t in np.arange(1,tmax):
+        ##### sst anomaly trend
+        tau[t]= b0 +b1 *(t+1990) +b2 *np.cos(t+1990) + b3 *np.sin(t+1990)
+
+        ##### catchability
+        valueScaled[t] = float(tau[t] - tauMin) / float(tauSpan)
+        q[t] = qMin + (valueScaled[t] * qSpan)
+
+        if q[t] > qc:
+            q[t] = qc
+            print "q high"
+        elif q[t] < 0:
+            q[t] = 0
+            print "q low"
+
+
+        ##### migration of squid
+        y_S[t]= yo[t] # timeseries from earlier input
+
+        if y_S[t] > 1:
+            y_S[t] = 1
+            print "yS high"
+        elif y_S[t] < 0.01:
+            y_S[t] = 0.01
+            print "yS low"
+
+        ##### trader cooperation
+        R_tt[t]= f+ np.exp(-d* y_S[t])
+
+        ##### squid population
+        if flag == 0: # squid population BEM
+            S[t] = S[t-1] +g *S[t-1] *(1- (S[t-1]/K)) - qc *E[t-1] *S[t-1]
+        if flag >= 1: # squid population MLM, BLM
+            S[t] = S[t-1] +g *S[t-1] *(1- (S[t-1]/K)) - q[t-1] *E[t-1] *S[t-1]
+
+        ## switch between models ##
+        if flag == 0: # effort BEM
+            Escal[t] = E[t-1] + p_f[t-1] *qc *E[t-1] *S[t-1] -c_t *E[t-1] # c_t is per trip so we need to upscale E hr > fisher > trip
+            E[t] = h1 *Escal[t] + h2 # Escal €[-3,10E+09; 1,60E+09]
+
+        if flag >= 1: # effort MLM, BLM
+            Escal[t] = E[t-1] + p_f[t-1] *q[t-1] *E[t-1] *S[t-1] -c_t *E[t-1] # c_t is per trip so we need to upscale E hr > fisher > trip
+            # fishing effort scaled
+            E[t] = h1 *Escal[t] + h2 # Escal €[-3,10E+09; 1,60E+09]
+
+        if E[t] > 1:
+            E[t] = 1
+            print "E high"
+        elif E[t] < 0:
+            E[t] = 0
+            print "E low"
+
+        ## switch between models ##
+        if flag == 0: # catch BEM
+            C[t] = qc *E[t] *S[t]
+        if flag >= 1: # catch MLM, BLM
+            C[t] = q[t] *E[t] *S[t]
+
+        if C[t] <= 0: # avoid infinity calculations
+            C[t]= 1
+
+        #### export price
+        p_e[t] = gamma* (C[t])**(-beta)
+
+        if p_e[t]>= 99366:
+            p_e[t]= 99366
+            print "pe high"
+
+
+        #### price for fishers
+        ## switch between models ##
+        # minimum wage
+        p_min[t]= (c_t *E[t])/C[t] #  MXN/ton
+
+        if flag == 0: # BEM
+            # price for fishers
+            p_f[t] = p_e[t] -c_p
+        if flag == 2: # BLM
+            p_f[t] = p_e[t] -c_p    # price for fishers
+        if flag == 1: #MLM
+            p_min[t]= (c_t *E[t])/C[t] # ->must be MXN/ton to fit into p_f calc
+            # price for fishers
+            p_f[t] = (p_e[t] -c_p) *(1-R_tt[t]) +R_tt[t] *p_min[t]
+
+        if p_f[t] >= (p_e[t] -c_p): # limit price of fishers
+            p_f[t] = (p_e[t] -c_p)
+
+        ##### revenue
+        # revenue of fishers
+        RF[t] = C[t] *p_f[t] -c_t *E[t]
+        # revenue of traders
+        RT[t] = C[t] *p_e[t] -RF[t] -c_p
+        # revenue of all fishery
+        RA[t] = C[t] *p_e[t]
+        # pay gap
+        G[t] = RF[t]/RT[t]
+
+        # print t, tau[t], ML[t], q[t], y_S[t], S[t], E[t], C[t], p_e[t], p_f[t]
+    return tau, ML, q, y_S, R_tt, S, E, C, p_e, p_f, RF, RT, RA, G
+
+
+################################################################################
+#############################  RUN MODEL FILE  #################################
+################################################################################
+
+##### Run the model ############################################################
+b1 = np.arange(0.013,0.027,0.0001)
+b2 = np.arange(5.25,8.75,0.025) ## continue here!!
+
+##### Initiate arrays ##########################################################
+cat = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save catches in each time period of each simulation
+pri = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save prices for fishers in each time period of each simulation
+mar = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save market prices in each time period of each simulation
+gap1 = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save price gap mean in each time period of each simulation
+gap2 = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save price gap stdv in each time period of each simulation
+tem = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save tau in each time period of each simulation
+mig = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save migrate squid in each time period of each simulation
+cco = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save catchability in each time period of each simulation
+pop = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save squid population in each time period of each simulation
+eff = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save effort in each time period of each simulation
+rff = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save revenue fishers in each time period of each simulation
+rtt = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save revenue traders in each time period of each simulation
+raa = np.zeros((b1.shape[0],b2.shape[0])) # matrix to save revenue all fishery in each time period of each simulation
+
+for i in np.arange(0,b1.shape[0]):
+    for j in np.arange(0,b2.shape[0]):
+        tau, q, y_S, R_tt, S, E, C, p_e, p_f, RF, RT, RA = model(b0, b1[i], b2[j], b3, l1, l2, a1, f, d, g, K, h1, h2, gamma, beta, c_p, c_t, flag)
+        gap1[i,j]= np.mean(p_f/p_e)
+        gap2[i,j]= np.std(p_f/p_e)
+
+        cat[i,j]= np.mean(C)
+        pri[i,j]= np.mean(p_f)
+        mar[i,j]= np.mean(p_e)
+
+        tem[i,j]= np.mean(tau)
+        cco[i,j]= np.mean(q)
+        pop[i,j]= np.mean(S)
+        eff[i,j]= np.mean(E)
+
+        rff[i,j]= np.mean(RF)
+        rtt[i,j]= np.mean(RT)
+        raa[i,j]= np.mean(RA)
+
+
+################################################################################
+################################  PLOT FILE  ###################################
+################################################################################
+
+##### Plot 1 ###################################################################
+## define dimensions
+y = b1 #  y axis
+x = b2 #  x axis
+z = mar #  output data
+## sub plot
+fig1 = plt.figure(figsize=[9,6])
+gs = gridspec.GridSpec(1,1,bottom=0.1,left=0.1,right=0.9)
+ax = fig1.add_subplot(gs[0,0])
+pcObject = ax.pcolormesh(x,y,z)
+plt.pcolormesh(x, y, z, cmap="Spectral")
+# both axis
+plt.tick_params(axis=1, which='major', labelsize=12)
+## set y-axis
+ax.set_ylabel('Trend $b1$', fontsize = 22)
+plt.ylim(0.013,0.026)
+## set x-axis
+ax.set_xlabel('Amplitude $b2$', fontsize = 22)
+plt.xlim(5.25,8.5)
+## colorbar
+cb = plt.colorbar()
+cb.set_label((r'mean price gap $\frac{P_f}{P_m}$'), rotation=270, labelpad=40, fontsize = 22)
+# plt.clim([0,1])
+## save and show
+# fig1.savefig("./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R_b1b2_mean.png",dpi=500)
+plt.show()
+
+################################################################################
+###################################  SI PLOTS ##################################
+################################################################################
+
+## define dimensions
+y = b1 #  y axis
+x = b2 #  x axis
+z = rff #  output data
+## sub plot
+fig1 = plt.figure(figsize=[9,6])
+gs = gridspec.GridSpec(1,1,bottom=0.1,left=0.1,right=0.9)
+ax = fig1.add_subplot(gs[0,0])
+pcObject = ax.pcolormesh(x,y,z)
+plt.pcolormesh(x, y, z, cmap="Spectral")
+# both axis
+plt.tick_params(axis=1, which='major', labelsize=12)
+## set y-axis
+ax.set_ylabel('Trend $b1$', fontsize = 22)
+plt.ylim(0.013,0.026)
+## set x-axis
+ax.set_xlabel('Amplitude $b2$', fontsize = 22)
+plt.xlim(5.25,8.5)
+## colorbar
+cb = plt.colorbar()
+cb.set_label('Revenue fishers $MXN$', rotation=270, labelpad=40, fontsize = 22)
+# plt.clim([0,1])
+## save and show
+# fig1.savefig("./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R_b1b2_SI_RF.png",dpi=500)
+plt.show()
+
+
+## define dimensions
+y = b1 #  y axis
+x = b2 #  x axis
+z = cat #  output data
+## sub plot
+fig1 = plt.figure(figsize=[9,6])
+gs = gridspec.GridSpec(1,1,bottom=0.1,left=0.1,right=0.9)
+ax = fig1.add_subplot(gs[0,0])
+pcObject = ax.pcolormesh(x,y,z)
+plt.pcolormesh(x, y, z, cmap="Spectral")
+# both axis
+plt.tick_params(axis=1, which='major', labelsize=12)
+## set y-axis
+ax.set_ylabel('Trend $b1$', fontsize = 22)
+plt.ylim(0.013,0.026)
+## set x-axis
+ax.set_xlabel('Amplitude $b2$', fontsize = 22)
+plt.xlim(5.25,8.5)
+## colorbar
+cb = plt.colorbar()
+cb.set_label('Catches $tons$', rotation=270, labelpad=40, fontsize = 22)
+# plt.clim([0,1])
+## save and show
+# fig1.savefig("./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/R_b1b2_SI_C.png",dpi=500)
+plt.show()
+
+################################################################################
+#############################  RUN TIMESERIES ##################################
+################################################################################
+#
+# OUT1 = np.zeros(tau.shape[0])
+# OUT2 = np.zeros(tau.shape[0])
+# OUT3 = np.zeros(tau.shape[0])
+# OUT4 = np.zeros(tau.shape[0])
+# OUT5 = np.zeros(tau.shape[0])
+# OUT6 = np.zeros(tau.shape[0])
+# OUT7 = np.zeros(tau.shape[0])
+# OUT8 = np.zeros(tau.shape[0])
+# OUT9 = np.zeros(tau.shape[0])
+#
+# for i in np.arange(0,1):
+#         tau, q, y_S, R_tt, S, E, C, p_e, p_f, RF, RT, RA = model(b0, b1, b2, b3, l1, l2, a1, f, d, g, K, h1, h2, gamma, beta, c_p, c_t, flag)
+#         OUT1[i]= p_f[i]
+#         OUT2[i]= C[i]
+#         OUT3[i]= tau[i]
+#         OUT4[i]= y_S[i]
+#         OUT5[i]= q[i]
+#         OUT6[i]= S[i]
+#         OUT7[i]= E[i]
+#         OUT8[i]= p_e[i]
+#
+#
+# ################################################################################
+# ###########################  PLOT FILE  ########################################
+# ################################################################################
+#
+# ### font ######################################################################
+# hfont = {'fontname':'Helvetica'}
+#
+# #####! PLOT MODEL  #############################################################
+# # Three subplots sharing both x/y axes
+# f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+# # axis 1
+# ax1.plot(tau, label = "tau", color="orange")
+# ax1.set_title('Temperature')
+# ax1.legend(loc="best")
+# # axis 2
+# ax2.plot(y_S, label = "migration", color="orange")
+# ax2.plot(q, label = "catchability", color="steelblue")
+# ax2.plot(E, label = "effort", color="red")
+# # ax2.plot(G, label = "pay gap", color="green")
+# ax2.legend(loc="best")
+# # axis 3
+# ax3.plot(C, label = "catch", color="orange")
+# ax3.plot(S, label = "population", color="steelblue")
+# ax3.legend(loc="best")
+# # axis 4
+# ax4.plot(p_f, label = "price for fishers", color="orange")
+# ax4.plot(p_e, label = "market price", color="steelblue")
+# ax4.legend(loc="best")
+# # Fine-tune figure; make subplots close to each other and hide x ticks for
+# # all but bottom plot.
+# f.subplots_adjust(hspace=0.2)
+# plt.show()
