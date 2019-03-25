c39c9c1c2bd6d4769c5da1cb603fffb2b2ba2cec
Lelsler <laura.elsler@su.se>
2019-03-25 21:54:04 +0100

    change colors of ML,M plotting

--- CODE/SI.py
+++ CODE/SI.py
@@ -5,14 +5,18 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
 import pandas as pd
-import scipy
+import scipy as sp
 from scipy.optimize import curve_fit
+import scipy.stats as stats
 from pandas import *
 hfont = {'fontname':'Helvetica'}
 
-#### Load dataset  #############################################################
+################################################################################
+###########################  LOAD DATA   #######################################
+################################################################################
+
+# load data for data plots
 df1 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/squid_all_plot_data.xlsx', sheetname='Sheet1')
-#! load columns
 yr = df1['year'] # year
 cat = df1['C'] # catches total from Tim data 1991-2014
 pg = df1['pf_GY'] # prices for fishers in Guaymas
@@ -26,131 +30,37 @@ cr = df1['C_RM'] # catches from datamares in all other offices except SR, GY
 cs = df1['C_SR'] # catches from datamares in Santa Rosalia
 ca = df1['C_ALL'] # catches from datamares summed over all offices
 
+# load data for price plots
 df2 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/squid_price_pfpe_ratio.xlsx', sheetname='Sheet1')
-#! load columns
 pm = df2['pm'] # comtrade price per ton
 psr = df2['pf_sr'] # fishers price SR
 pgy = df2['pf_gy'] # fishers price SR
 pre = df2['pf_re'] # fishers price Remaining offices
 
-### Plot files #################################################################
-### catches
-fig = plt.figure()
-# add the first axes using subplot
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(cat, label = "Total", color="black")
-line2, = ax1.plot(cg, label = "Guaymas", color = "blue")
-line3, = ax1.plot(cs, label = "Santa Rosalia", color = "red")
-# x-axis
-ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
-# ax1.set_xlim(0,)
-# plt.xlabel("Year",fontsize=20, **hfont)
-# y-axis
-plt.ylabel("Catches $tons$", rotation=90, labelpad=5, fontsize=20, **hfont)
-# legend
-plt.legend([line1, line2, line3], ["Total", "Guaymas", "Santa Rosalia"], fontsize= 11)
-# save and show
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_catch.png',dpi=500)
-plt.show()
+# load data for optimization plots
+eff= np.load("./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/parameter_sweep_effort.npy")
+#! optimization effort
+df3 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/BEM_effort.xlsx', sheetname='Sheet1')
+efo = df3['bem_effort'] # effort optimization
+#! optimization catch
+df4 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/model_compare_C.xlsx', sheetname='Sheet1')
+bemc = df4['BEM'] # BEM catch predictions
+edmc = df4['EDM'] # EDM catch predictions
+mlmc = df4['MLM'] # MLM catch predictions
+datc = df4['Data'] # data
+#! optimization price
+df5 = pd.read_excel('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/DATA/model_compare_pf.xlsx', sheetname='Sheet1')
+bemp = df5['BEM'] # BEM price predictions
+edmp = df5['EDM'] # EDM price predictions
+mlmp = df5['MLM'] # MLM price predictions
+datp = df5['Data'] # data
 
-### prices
-fig = plt.figure()
-# add the first axes using subplot
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(pg, label = "Guaymas", color = "blue")
-line2, = ax1.plot(ps, label = "Santa Rosalia", color = "red")
-line3, = ax1.plot(pr, label = "Other offices", color="black")
-# x-axis
-ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
-ax1.set_xlim(0,len(yr))
-# plt.xlabel("Year",fontsize=20, **hfont)
-# y-axis
-plt.ylabel("Fishers' price $MXN$", rotation=90, labelpad=5, fontsize=20, **hfont)
-# legend
-plt.legend([line3, line2, line1], ["Remaining offices", "Guaymas", "Santa Rosalia"], fontsize= 11, loc="best")
-# save and show
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_price.png',dpi=500)
-plt.show()
+################################################################################
+###########################  PLOT FILE   #######################################
+################################################################################
 
-### migration
-fig = plt.figure()
-# add the first axes using subplot
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(ys, color = "black")
-# x-axis
-ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
-ax1.set_xlim(0,len(yr))
-# y-axis
-plt.ylabel("Proportion of migrated squid", rotation=90, labelpad=5, fontsize=20, **hfont)
-# save and show
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_M.png',dpi=500)
-plt.show()
-
-### mantle length
-fig = plt.figure()
-# add the first axes using subplot
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(ml, color = "black")
-# x-axis
-ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
-ax1.set_xlim(0,len(yr))
-# y-axis
-plt.ylabel("Mantle length $cm$", rotation=90, labelpad=5, fontsize=20, **hfont)
-# save and show
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_ML.png',dpi=500)
-plt.show()
-
-
-### sst anomaly
-fig = plt.figure()
-# add the first axes using subplot
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(sst, color = "black")
-# x-axis
-ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
-ax1.set_xlim(0,len(yr))
-# plt.xlabel("Year",fontsize=20, **hfont)
-# y-axis
-plt.ylabel("SST anomaly", rotation=90, labelpad=5, fontsize=20, **hfont)
-# save and show
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_sst.png',dpi=500)
-plt.show()
-
-### prices and catch scatter
-x = range(0,130000)
-y = range(0,18000)
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.scatter(cs,ps, s= 30, c='red', marker='o', label='Santa Rosalia')
-ax1.scatter(cg,pg, s= 30, c='blue', marker='o', label='Guaymas')
-ax1.scatter(cr,pr, s= 30, c='black', marker='o', label='Remaining offices')
-# axis
-ax1.set_xlim(0,5E4)
-plt.xlabel("Catch $tons$",fontsize=20)
-plt.ylabel("Fishers' price $MXN$",fontsize=20)
-plt.legend(loc="best", fontsize=12);
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_scatterprice.png',dpi=500)
-plt.show()
-
-### ML and catch scatter
-slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(ml[5:24], cat[5:24])
-print("r-squared ml catch:", r_value**2)
-scipy.stats.pearsonr(ml[5:24], cat[5:24])
-# begin fit
-x = ml[5:24]
-y = cat[5:24]
-fit = np.polyfit(x,y,1)
-fit_fn = np.poly1d(fit)  # fit_fn is now a function which takes in x and returns an estimate for y
-# plot function
-plt.plot(x,y, 'yo', x, fit_fn(x), '--k')
-plt.xlim(20, 80)
-plt.xlabel("Mantle length $cm$",fontsize=20)
-plt.ylim(0, 130E3)
-plt.ylabel("Catch $tons$",fontsize=20)
-# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_scatterml.png',dpi=500)
-plt.show()
-
-### SST anomaly
+############## MAIN TEXT MAP ###################################################
+#! SST anomaly
 fig = plt.figure()
 # add the first axes using subplot populated with predictions
 ax1 = fig.add_subplot(111)
@@ -173,7 +83,148 @@ plt.legend([line1, line2], ["Catch", "SST anomaly"], fontsize= 12)
 # fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_sstcatch.png',dpi=500)
 plt.show()
 
-### price gap
+
+######## SI OPTIMIZATION #######################################################
+#! optimization catch
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(bemc, label = "BEM", color="steelblue")
+line2, = ax1.plot(edmc, label = "EDM", color="sage", linewidth=3)
+line3, = ax1.plot(mlmc, label = "MLM", color="orange")
+line4, = ax1.plot(datc, label = "Data", color="indianred", linewidth =2)
+# x-axis
+ax1.set_xticklabels(np.arange(1996,2017,5), rotation=45, fontsize= 12)
+# plt.xlabel("Year",fontsize=20, **hfont)
+# y-axis
+plt.ylabel("Catch $tons$", rotation=90, labelpad=5, fontsize=20, **hfont)
+# legend
+plt.legend([line1, line2,line3,line4], ["BEM","EDM","MLM","Data"], fontsize= 12)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_optic.png',dpi=500)
+plt.show()
+
+#! optimization fishers price
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(bemp, label = "BEM", color="steelblue", linewidth=2)
+line2, = ax1.plot(edmp, label = "EDM", color="sage", linewidth=2)
+line3, = ax1.plot(mlmp, label = "MLM", color="orange", linewidth=2)
+line4, = ax1.plot(datp, label = "Data", color="indianred", linewidth=2)
+line5, = ax1.plot(datc+5E5, label = "Data", color="white")
+# x-axis
+ax1.set_xticklabels(np.arange(1996,2017,5), rotation=45, fontsize= 12)# ax1.set_xlim(0,)
+# plt.xlabel("Year",fontsize=20, **hfont)
+# y-axis
+plt.ylabel("Fishers' price $MXN$", rotation=90, labelpad=5, fontsize=20, **hfont)
+ax1.set_ylim(0,3.5E4)
+# legend
+plt.legend([line1, line2,line3,line4], ["BEM","EDM","MLM","Data"], loc='best', fontsize= 12)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_optipf.png',dpi=500)
+plt.show()
+
+#! effort
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(efo, color = "black", linewidth=2)
+line2, = ax1.plot(eff[6:], color = "red", linewidth=2)
+# x-axis
+ax1.set_xticklabels(np.arange(1996,2017,5), rotation=45, fontsize= 12)
+# y-axis
+plt.ylabel("Effort", rotation=90, labelpad=5, fontsize=20, **hfont)
+plt.legend([line1,line2], ["Optimization","Simulation"], loc='best', fontsize= 12)
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_effort.png',dpi=500)
+plt.show()
+
+############## SI STATES #######################################################
+#! catches
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(cat, label = "Total", color="black")
+line2, = ax1.plot(cg, label = "Guaymas", color = "blue")
+line3, = ax1.plot(cs, label = "Santa Rosalia", color = "red")
+# x-axis
+ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
+# ax1.set_xlim(0,)
+# plt.xlabel("Year",fontsize=20, **hfont)
+# y-axis
+plt.ylabel("Catches $tons$", rotation=90, labelpad=5, fontsize=20, **hfont)
+# legend
+plt.legend([line1, line2, line3], ["Total", "Guaymas", "Santa Rosalia"], fontsize= 11)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_catch.png',dpi=500)
+plt.show()
+
+### fishers price
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(pg, label = "Guaymas", color = "blue")
+line2, = ax1.plot(ps, label = "Santa Rosalia", color = "red")
+line3, = ax1.plot(pr, label = "Other offices", color="black")
+# x-axis
+ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
+ax1.set_xlim(0,len(yr))
+# plt.xlabel("Year",fontsize=20, **hfont)
+# y-axis
+plt.ylabel("Fishers' price $MXN$", rotation=90, labelpad=5, fontsize=20, **hfont)
+# legend
+plt.legend([line3, line2, line1], ["Remaining offices", "Guaymas", "Santa Rosalia"], fontsize= 11, loc="best")
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_price.png',dpi=500)
+plt.show()
+
+### mantle length
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(ml, color = "black")
+# x-axis
+ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
+ax1.set_xlim(0,len(yr))
+# y-axis
+plt.ylabel("Mantle length $cm$", rotation=90, labelpad=5, fontsize=20, **hfont)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_ML.png',dpi=500)
+plt.show()
+
+### sst anomaly
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(sst, color = "black")
+# x-axis
+ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
+ax1.set_xlim(0,len(yr))
+# plt.xlabel("Year",fontsize=20, **hfont)
+# y-axis
+plt.ylabel("SST anomaly", rotation=90, labelpad=5, fontsize=20, **hfont)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_sst.png',dpi=500)
+plt.show()
+
+
+############## SI PRICES #######################################################
+#! prices and catch scatter
+x = range(0,130000)
+y = range(0,18000)
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.scatter(cs,ps, s= 30, c='red', marker='o', label='Santa Rosalia')
+ax1.scatter(cg,pg, s= 30, c='blue', marker='o', label='Guaymas')
+ax1.scatter(cr,pr, s= 30, c='black', marker='o', label='Remaining offices')
+# axis
+ax1.set_xlim(0,5E4)
+plt.xlabel("Catch $tons$",fontsize=20)
+plt.ylabel("Fishers' price $MXN$",fontsize=20)
+plt.legend(loc="best", fontsize=12);
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_scatterprice.png',dpi=500)
+plt.show()
+
+#! price gap
 x = range(18000)
 y = range(5000)
 fig = plt.figure()
@@ -187,4 +238,41 @@ plt.xlabel("Fishers' price $MXN$",fontsize=20)
 plt.ylabel("Market price $MXN$",fontsize=20)
 plt.legend(loc="best", fontsize=12);
 # fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_pmpe.png',dpi=500)
+plt.show()
+
+
+
+
+################### NOT USED ###################################################
+
+### ML and catch scatter
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(ml[5:24], cat[5:24])
+print("r-squared ml catch:", r_value**2)
+scipy.stats.pearsonr(ml[5:24], cat[5:24])
+# begin fit
+x = ml[5:24]
+y = cat[5:24]
+fit = np.polyfit(x,y,1)
+fit_fn = np.poly1d(fit)  # fit_fn is now a function which takes in x and returns an estimate for y
+# plot function
+plt.plot(x,y, 'yo', x, fit_fn(x), '--k')
+plt.xlim(20, 80)
+plt.xlabel("Mantle length $cm$",fontsize=20)
+plt.ylim(0, 130E3)
+plt.ylabel("Catch $tons$",fontsize=20)
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_scatterml.png',dpi=500)
+plt.show()
+
+### migration
+fig = plt.figure()
+# add the first axes using subplot
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(ys, color = "black")
+# x-axis
+ax1.set_xticklabels(np.arange(1991,2017,5), rotation=45, fontsize= 12)
+ax1.set_xlim(0,len(yr))
+# y-axis
+plt.ylabel("Proportion of migrated squid", rotation=90, labelpad=5, fontsize=20, **hfont)
+# save and show
+# fig.savefig('./Dropbox/PhD/Resources/Squid/Squid/CODE/Squid/FIGS/SI_M.png',dpi=500)
 plt.show()
